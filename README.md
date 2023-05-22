a3ff81e1f10bb0bd16fcba452c4c29a7e5e075e7
pssvlng <info@passivlingo.com>
2023-05-22 07:49:08 +0200

    Curriculum Sample Data and Shacl Validations

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# curriculum-owl
+# Building Dini Curriculum Ontology
+
+# Requirements
+- Python3
+
+# Installation
+```
+pip3 install owlready2
+```
+# Execution
+
+## Running the Application
+```
+python3 dini.py
+```
+## Outputs
+The application creates an ontology ```dinio.owl``` in the application directory. This is the complete ontology and can be opened and viewed inside an ontology editor such as Protege.
+
+A folder ```Export``` is also created in the application directory, and it contains the dependent files for the ```dinio.owl``` ontology.
--- dini.py
+++ dini.py
@@ -8,13 +8,14 @@ Mathe_Primar_File_Names = ["iqb_Mathematik_Primar_ab", "iqb_Mathematik_Primar_st
 Mathe_Sek1_File_Names = ["iqb_Mathe_Sek_1_stdInhaltESA", "iqb_Mathe_Sek_1_stdInhaltMSA", "iqb_Mathe_Sek_1_ab", "iqb_Mathe_Sek_1_stdProzess"]
 Fremdsprachen_Sek1_File_Names = ["iqb_Fremdsprachen_Sek_I"]
 Educational_Level_File_Names = ["educationalLevel"]
+Sample_Data = ["CurriculumData"]
 skos_core = "http://www.w3.org/2004/02/skos/core#"
 
 def get_ontology_file_path(path_base, file): 
     return f"{path_base}{file}"
 
 def load_imported_ontologies(onto, file_base):    
-    file_list = Deutsch_Sek1_File_Names + Deutsch_Primar_File_Names + Educational_Level_File_Names + Mathe_Primar_File_Names + Mathe_Sek1_File_Names + Fremdsprachen_Sek1_File_Names
+    file_list = Deutsch_Sek1_File_Names + Deutsch_Primar_File_Names + Educational_Level_File_Names + Mathe_Primar_File_Names + Mathe_Sek1_File_Names + Fremdsprachen_Sek1_File_Names + Sample_Data
     for file_name in file_list:
         onto.imported_ontologies.append(get_ontology(f"{file_base}{file_name}.rdf").load())        
     
@@ -50,43 +51,26 @@ def create_ontology():
             pass
 
         class EducationalLevel(Thing):
-            pass    
-
-        class Curriculum(Thing):
-            pass    
-
-        class CurriculumItem(Thing):
-            pass
-
-        class educationalStandard(ObjectProperty):
-            domain = [Curriculum, CurriculumItem]
-            range = [Bildungsstandard]
-
-        class educationalLevel(ObjectProperty):
-            domain = [Curriculum]
-            range = [EducationalLevel]    
+            pass            
 
         class assesses(ObjectProperty):            
-            range = [CurriculumItem]    
+            pass
 
-        class assessedBy(ObjectProperty):
-            domain = [CurriculumItem]
+        class assessedBy(ObjectProperty):            
             inverse_property = assesses            
 
-        class taught(ObjectProperty):            
-            range = [CurriculumItem]    
+        class teaches(ObjectProperty):            
+            pass            
 
-        class taughtBy(ObjectProperty):
-            domain = [CurriculumItem] 
-            inverse_property = taught               
+        class taughtBy(ObjectProperty):            
+            inverse_property = teaches               
 
-        AllDisjoint([Bildungsstandard, EducationalLevel, Curriculum, CurriculumItem])
-        AllDisjoint([Bildungsstandard2022_Deutsch_Primar, Bildungsstandard2022_Deutsch_Sek1, Bildungsstandard2022_Mathe_Primar, Bildungsstandard2022_Mathe_Sek1, Bildungsstandard2022_Fremdsprachen_Sek1])
-
+        AllDisjoint([Bildungsstandard2022_Deutsch_Primar, Bildungsstandard2022_Deutsch_Sek1, Bildungsstandard2022_Mathe_Primar, Bildungsstandard2022_Mathe_Sek1, Bildungsstandard2022_Fremdsprachen_Sek1])        
+    
     onto_skos = get_ontology(skos_core).load()
-    broader = next((x for x in onto_skos.object_properties() if str(type(x) == 'core.broader')), None)     
-    broader.is_a.append(ObjectProperty)
-    CurriculumItem.equivalent_to = [broader.some(Curriculum), broader.some(CurriculumItem)]     
+    skos_obj_props = [x for x in onto_skos.object_properties() if x.name in ['broader', 'narrower', 'closeMatch']]
+    for prop in skos_obj_props:
+        prop.is_a.append(ObjectProperty)     
 
     for imported_onto in onto.imported_ontologies:
         if imported_onto.name in Deutsch_Sek1_File_Names:
@@ -100,35 +84,60 @@ def create_ontology():
         if imported_onto.name in Fremdsprachen_Sek1_File_Names:
             append_class_to_core_concept(imported_onto, Bildungsstandard2022_Fremdsprachen_Sek1)                 
         if imported_onto.name in Educational_Level_File_Names:
-            append_class_to_core_concept(imported_onto, EducationalLevel)        
-        
+            append_class_to_core_concept(imported_onto, EducationalLevel)                
+        if imported_onto.name == 'CurriculumData':
+            curriculum = next((x for x in imported_onto.classes() if x.name == 'Curriculum'), None)            
+            curriculum_item = next((x for x in imported_onto.classes() if x.name == 'CurriculumItem'), None)            
+            
+            broader = next((x for x in skos_obj_props if x.name == 'broader'), None)     
+            #narrower = next((x for x in skos_obj_props if x.name == 'narrower'), None)         
+            #curriculum_item.equivalent_to = [broader.some(curriculum), broader.some(curriculum_item), narrower.some(curriculum_item)]                
+            curriculum_item.equivalent_to = [broader.some(curriculum_item), broader.some(curriculum)]                
+
+            educationalLevel = next((x for x in imported_onto.object_properties() if x.name == 'educationalLevel'), None)            
+            educationalStandard = next((x for x in imported_onto.object_properties() if x.name == 'educationalStandard'), None)            
+            #educationalStandard.domain = [curriculum, curriculum_item]            
+            educationalLevel.domain = [curriculum]
+            educationalStandard.range = [Bildungsstandard]                        
+            educationalLevel.range = [EducationalLevel]
+            assesses.range = [curriculum_item]
+            assessedBy.domain = [curriculum_item]
+            teaches.range = [curriculum_item]
+            taughtBy.domain = [curriculum_item]
+
+            AllDisjoint([Bildungsstandard, EducationalLevel, curriculum, curriculum_item])
+                    
     return onto
     
 def save_ontology(main_onto, file_name):    
-    if not os.path.exists("/Export"):
+    if not os.path.exists("Export"):
         os.makedirs("Export")
-    for ontology in main_onto.imported_ontologies:
+    for ontology in main_onto.imported_ontologies:  
         file_path = get_ontology_file_path(Export_Base, f'{ontology.name}.rdf')
         ontology.save(file = file_path, format = "rdfxml")
 
-    main_onto.save(file = file_name, format = "rdfxml")     
+    main_onto.save(file = file_name, format = "rdfxml")  
+
+    return main_onto   
 
 def replace_text_in_file(file_name, in_text, out_text):
     with open(file_name, 'r') as file :
         filedata = file.read()        
     filedata = filedata.replace(in_text, out_text)     
     with open(file_name, 'w') as file:
-        file.write(filedata)
-        
-    
+        file.write(filedata)        
+
+
+# Main Program
+
 onto = create_ontology()
 target_file = "dinio.owl"
-save_ontology(onto, target_file)
+onto = save_ontology(onto, target_file)
 
 replace_in_text = '<owl:imports rdf:resource="'
 replace_out_text = '<owl:imports rdf:resource="file://'
 replace_text_in_file(target_file, replace_in_text, replace_out_text)
 replace_text_in_file(target_file, Import_Base, Export_Base)
 
-
+# End Main Program
 
